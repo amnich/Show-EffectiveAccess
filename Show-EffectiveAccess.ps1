e82f6fc2b131c6c4392312b4795cf957ce479fd4
Adam Mnich <adammnich@gmail.com>
2017-06-02 12:45:14 +0200

    Update Show-EffectiveAccess.ps1

--- Show-EffectiveAccess.ps1
+++ Show-EffectiveAccess.ps1
@@ -1,7 +1,7 @@
 function Show-EffectiveAccess {
     <#
 	.SYNOPSIS
-	Show GUI with effective access right of a user on directories on given path as a tree.
+	Show GUI with effective access rights of a user on directories on given path as a tree.
 	Module PowerShellAccessControl is required.
 	https://github.com/PowerShellOrg/PowerShellAccessControl
 	
